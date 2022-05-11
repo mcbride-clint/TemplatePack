32aa9857f91433f94d18bdf3a0ff04d14ee496aa
mcbride-clint <cmm5559@gmail.com>
2022-05-11 16:06:22 -0400

    Create basic example item template

---
+++ example-templates/item-templates/middleware/middleware.cs
@@ -0,0 +1,31 @@
+namespace middleware
+{
+    public class RequestCultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestCultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Perform Needed Actions Before Request
+
+            // Call the next delegate/middleware in the pipeline.
+            await _next(context);
+
+            // Perform Needed Actions After Request
+        }
+    }
+
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+}
